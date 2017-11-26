0b2213d6e8198fd950ef9ade3ed19bbc202043f4
raviprajapat <raviprajapat25@hotmail.com>
2017-11-26 22:16:01 +0530

    export to excel

---
+++ index.js
@@ -0,0 +1,24 @@
+var jsreport = require('jsreport-core')();
+var fs = require('fs');
+var path = require('path');
+jsreport.use(require('jsreport-html-to-xlsx')());
+jsreport.use(require('jsreport-jsrender')());
+var template = path.join(__dirname,'template');
+
+jsreport.init().then(function () {     
+   return jsreport.render({
+	   template: {
+		   content: '<table><tr><td>Hello {{:foo}}</td></tr></table>',
+		   engine: 'jsrender',
+		   recipe: 'html-to-xlsx'
+		},
+		data: {
+			foo: "world"
+		}
+	}).then(function(resp) {
+	 //prints pdf with headline Hello world
+     resp.result.pipe(fs.createWriteStream(path.join(template,"myexcel.xlsx")));
+   });
+}).catch(function(e) {
+  console.log(e)
+})
